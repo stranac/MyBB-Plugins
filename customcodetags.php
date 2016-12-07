5d2dba5200609d4b82663cb84b830e744ce63268
stranac <stranac@hotmail.com>
2016-12-07 19:18:20 +0100

    Refactored output code and fixed a few bugs

--- customcodetags.php
+++ customcodetags.php
@@ -20,16 +20,18 @@ function customcodetags_info()
 }
 
 $plugins->add_hook("parse_message_start", "parse_custom_tags");
+$plugins->add_hook("parse_message_end", "fix_newlines");
 
 
 function parse_custom_tags($message)
 {
+	global $custom_code_matches;
 	$pattern = "#\[(code|php|python|output|error)\](.*?)\[/\\1\]|\[icode\]([^\n]*?)\[/icode\]#si";
 
-	preg_match_all($pattern, $message, $code_matches, PREG_SET_ORDER);
+	preg_match_all($pattern, $message, $custom_code_matches, PREG_SET_ORDER);
 	$message = preg_replace($pattern, "<mybb-code>\n", $message);
 
-	foreach ($code_matches as $text) {
+	foreach ($custom_code_matches as $text) {
 		$type = my_strtolower($text[1]);
 
 		// let mybb handle code and php tags
@@ -48,16 +50,36 @@ function parse_custom_tags($message)
 			}
 
 			// escape brackets to avoid further parsing
-			// replace newlines with <br> tags, so nl2br() doesn't mess with spacing in our <pre> tags
-			$content = str_replace(array("[", "]", "<", ">", "\n"), array("&#91;","&#93;", "&lt;", "&gt;", "<br />"), $content);
+			$content = str_replace(array("[", "]", "<", ">"), array("&#91;","&#93;", "&lt;", "&gt;"), $content);
+			// horrible hack to avoid nl2br being executed on our code blocks
+			$content = str_replace("\n", "<newline>", $content);
 
-			if ($type == "python") {
-				$message = preg_replace("#<mybb-code>\n#", "<pre class=\"brush: python\">".$content."</pre>", $message, 1);
-			}
-			else {
-				$message = preg_replace("#<mybb-code>\n#", "<code class=\"".$type."\">".$content."</code>", $message, 1);
-			}
+			$message = preg_replace("#<mybb-code>\n#", format_code($type, $content), $message, 1);
 		}
 	}
 	return $message;
+}
+
+
+function format_code($type, $content)
+{
+	if ($type == "python") {
+		return "<b>Python Code:</b><pre class=\"brush: python\">".$content."</pre>";
+	}
+
+	$code_tag = "";
+
+	if ($type == "output" || $type == "error") {
+			$code_tag .= "<b>".ucfirst($type).":</b>";
+		}
+		$code_tag .= "<code class=\"".$type."\">";
+
+	return $code_tag.$content."</code>";
+
+}
+
+
+function fix_newlines($message) {
+	// get our newlines back
+	return str_replace("<newline>", "\n", $message);
 }
